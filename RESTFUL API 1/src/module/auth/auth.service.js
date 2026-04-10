689e2ea9a0e63239867cde3e7a95f3450efb27c6
Sanghita Seal <sealsanghita2005@gmail.com>
2026-04-10 09:46:09 +0530

    Implement refresh token functionality with error handling in auth.service.js

--- RESTFUL API 1/src/module/auth/auth.service.js
+++ RESTFUL API 1/src/module/auth/auth.service.js
@@ -3,11 +3,13 @@ import {
   generateAccessToken,
   generateResetToken,
   generateRefreshToken,
+  verifyRefreshToken,
 } from "../../common/utils/jwt.utils.js";
 import User from "./auth.model.js";
 
 //2
-const hashToken = (token) => crypto.createHash("sha256").update(token).digest("hex");
+const hashToken = (token) =>
+  crypto.createHash("sha256").update(token).digest("hex");
 
 const register = async ({ name, email, password, role }) => {
   const existing = await User.findOne({ email });
@@ -54,13 +56,37 @@ const login = async ({ email, password }) => {
   //3
   user.refreshToken = hashToken(refreshToken); // We are doing it in copy
   //4 save in DB
-  await user.save({validateBeforeSave: false}) // otherwise mongoose will validate all data again
+  await user.save({ validateBeforeSave: false }); // otherwise mongoose will validate all data again
 
-  const userObj = user.toObject()
-  delete userObj.password
-  delete userObj.refreshToken
+  const userObj = user.toObject();
+  delete userObj.password;
+  delete userObj.refreshToken;
 
-  return {user: userObj, accessToken, refreshToken  }
+  return { user: userObj, accessToken, refreshToken };
+};
+
+//5
+const refresh = async (token) => {
+  if (!token) throw ApiError.unauthorised("Refresh Token missing");
+  const decoded = verifyRefreshToken(token);
+
+  const user = await User.findById(decoded.id).select("+refreshToken");
+  if (!user) throw ApiError.unauthorised("User not found");
+
+  if (user.refreshToken !== hashToken(token))
+    throw ApiError.unauthorised("Invalid Refresh Token");
+
+  const accessToken = generateAccessToken({ id: user._id, role: user.role });
+  const refreshToken = generateRefreshToken({ id: user._id, role: user.role });
+  user.refreshToken = hashToken(refreshToken); // We are doing it in copy
+  //4 save in DB
+  await user.save({ validateBeforeSave: false }); // otherwise mongoose will validate all data again
+
+  const userObj = user.toObject();
+  delete userObj.password;
+  delete userObj.refreshToken;
+
+  retrun({ accessToken });
 };
 
 export { register };
